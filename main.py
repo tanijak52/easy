e2bbed5ba8ba924c1a54b6700cc1b9147fb794fe
tanijak52 <tatuana7887yy@gmail.com>
2025-04-17 10:50:19 +0200

    add ui, button, layout, widgets

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("hcdj")
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QLabel, QPushButton, QListWidget, QHBoxLayout, QVBoxLayout
+
+app = QApplication([])
+win = QWidget()
+win.resize(700, 500)
+imege = QLabel("Картинка")
+btn = QPushButton("Папка")
+files = QListWidget()
+
+btn_left = QPushButton("Вліво")
+btn_right = QPushButton("Вправо")
+btn_mirror = QPushButton("Дзеркало")
+btn_sharp = QPushButton("Різкість")
+btn_bw = QPushButton("Ч/Б")
+
+a = QHBoxLayout()
+b = QVBoxLayout()
+c = QVBoxLayout()
+
+b.addWidget(btn)
+b.addWidget(files)
+c.addWidget(imege, alignment=Qt.AlignCenter)
+
+a_tools = QHBoxLayout()
+a_tools.addWidget(btn_left)
+a_tools.addWidget(btn_right)
+a_tools.addWidget(btn_mirror)
+a_tools.addWidget(btn_sharp)
+a_tools.addWidget(btn_bw)
+
+c.addLayout(a_tools)
+
+a.addLayout(b, 1)  
+a.addLayout(c, 3) 
+win.setLayout(a)
+win.show()
+app.exec()
