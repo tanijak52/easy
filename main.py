f52773a6095f88ca2c8ae0b0d835250a6f63ab85
tanijak52 <tatuana7887yy@gmail.com>
2025-04-25 19:59:52 +0200

    add class and def

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QLabel, QPushButton, QListWidget, QHBoxLayout, QVBoxLayout
+from PyQt5.QtGui import QPixmap
 import os
+
 app = QApplication([])
 win = QWidget()
 win.resize(700, 500)
-imege = QLabel("Картинка")
+image = QLabel("Картинка")
 btn = QPushButton("Папка")
 files = QListWidget()
+workdir = "None"
 
 btn_left = QPushButton("Вліво")
 btn_right = QPushButton("Вправо")
@@ -18,9 +21,10 @@ hl_a = QHBoxLayout()
 vl_b = QVBoxLayout()
 vl_c = QVBoxLayout()
 
+
 vl_b.addWidget(btn)
 vl_b.addWidget(files)
-vl_c.addWidget(imege, alignment=Qt.AlignCenter)
+vl_c.addWidget(image, 95 )
 
 hl_a_tools = QHBoxLayout()
 hl_a_tools.addWidget(btn_left)
@@ -35,9 +39,39 @@ hl_a.addLayout(vl_b, 1)
 hl_a.addLayout(vl_c, 3) 
 win.setLayout(hl_a)
 
-workdir = "None"
+class ImageProcessor():
+    def __init__(self):
+        self.image = None
+        self.dir = None
+        self.filename = None
+        self.save_dir = "Modified/"
+        self.fullname = None
 
-def filter(filtres, extensions):
+    def loadImage(self, dir, filename):
+        self.filename = filename
+        path = os.path.join(self.dir, self.filename)
+        self.dir = dir
+        self.image = path
+        self.fullname = path
+    def showImage(self):
+        image.hide()
+        pixmapimage = QPixmap(self.fullname)
+        w, h = image.width(), image.height()
+        pixmapimage =  pixmapimage.scaled(w, h, Qt.KeepAspectRatio)
+        image.setPixmap(pixmapimage)
+        image.show()
+    def saveImage(self):
+        save_path = os.path.join(self.dir, self.save_dir)
+        if not(os.path.exists(save_path) or os.path.isdir(save_path)): 
+            os.mkdir(save_path)
+        self.image.save(os.path.join(save_path, self.filename))
+    def do_bw(self):
+        self.image = self.image.convert("L")
+        self.saveImage()
+        image_path = os.path.join(self.dir, self.save_dir, self.filename)
+        self.showImage(image_path)
+
+def filter(files, extensions):
     result = []
 
     for file in files:
@@ -45,22 +79,31 @@ def filter(filtres, extensions):
             if file.endswith(ext):
                 result.append(file)
     return result 
+
 def ChosseWorkdir():
     global workdir
     workdir = QFileDialog.getExistingDirectory()
 
     print("відкрита папка: " +workdir)
+
 def ShowFiles():
-    extensions = ['.jpg','.jpeg', '.png', '.gif', '.bmp']
+    extensions = ['.jpg','.jpeg', '.png', '.gif', '.bmp', '.avif']
     ChosseWorkdir()
     filenames = filter(os.listdir(workdir),extensions)
     files.clear()
     for filename in filenames:
         files.addItem(filename)
 
+workImage = ImageProcessor()
 
+def showChosenImage():
+    if files.currentRow () >= 0:
+        filename = files.currentItem().text()
+        workImage.loadImage(workdir, filename)
+        workImage.showImage()
 
-
+files.currentRowChanged.connect(showChosenImage)
 btn.clicked.connect(ShowFiles)
+do_bw.clicked.connect(workImage.do_bw)
 win.show()
 app.exec()
