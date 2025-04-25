4eb5d8d03ed1e0ebb06915e117819fe578e67649
tanijak52 <tatuana7887yy@gmail.com>
2025-04-25 16:00:54 +0200

    add def and for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QLabel, QPushButton, QListWidget, QHBoxLayout, QVBoxLayout
-
+import os
 app = QApplication([])
 win = QWidget()
 win.resize(700, 500)
@@ -14,25 +14,53 @@ btn_mirror = QPushButton("Дзеркало")
 btn_sharp = QPushButton("Різкість")
 btn_bw = QPushButton("Ч/Б")
 
-a = QHBoxLayout()
-b = QVBoxLayout()
-c = QVBoxLayout()
+hl_a = QHBoxLayout()
+vl_b = QVBoxLayout()
+vl_c = QVBoxLayout()
 
-b.addWidget(btn)
-b.addWidget(files)
-c.addWidget(imege, alignment=Qt.AlignCenter)
+vl_b.addWidget(btn)
+vl_b.addWidget(files)
+vl_c.addWidget(imege, alignment=Qt.AlignCenter)
 
-a_tools = QHBoxLayout()
-a_tools.addWidget(btn_left)
-a_tools.addWidget(btn_right)
-a_tools.addWidget(btn_mirror)
-a_tools.addWidget(btn_sharp)
-a_tools.addWidget(btn_bw)
+hl_a_tools = QHBoxLayout()
+hl_a_tools.addWidget(btn_left)
+hl_a_tools.addWidget(btn_right)
+hl_a_tools.addWidget(btn_mirror)
+hl_a_tools.addWidget(btn_sharp)
+hl_a_tools.addWidget(btn_bw)
 
-c.addLayout(a_tools)
+vl_c.addLayout(hl_a_tools)
 
-a.addLayout(b, 1)  
-a.addLayout(c, 3) 
-win.setLayout(a)
+hl_a.addLayout(vl_b, 1)  
+hl_a.addLayout(vl_c, 3) 
+win.setLayout(hl_a)
+
+workdir = "None"
+
+def filter(filtres, extensions):
+    result = []
+
+    for file in files:
+        for ext in extensions:
+            if file.endswith(ext):
+                result.append(file)
+    return result 
+def ChosseWorkdir():
+    global workdir
+    workdir = QFileDialog.getExistingDirectory()
+
+    print("відкрита папка: " +workdir)
+def ShowFiles():
+    extensions = ['.jpg','.jpeg', '.png', '.gif', '.bmp']
+    ChosseWorkdir()
+    filenames = filter(os.listdir(workdir),extensions)
+    files.clear()
+    for filename in filenames:
+        files.addItem(filename)
+
+
+
+
+btn.clicked.connect(ShowFiles)
 win.show()
 app.exec()
